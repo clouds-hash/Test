55223c9c2784e1a2c765c28f1b3142114eb32c33
Clouds <3190561741@qq.com>
2026-05-31 19:34:25 +0800

    "第二次修改"

--- MYTestForGit/src/App.java
+++ MYTestForGit/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("This is a test.");
+        System.out.println("This is another line.");
+
+        System.out.println("This is yet another line.");
+        System.out.println("This is the last line.");
     }
 }
